87236b48aa1f335452ccdaf7749870bf5c593968
tash-01 <tashfernandes93@gmail.com>
2023-03-16 15:49:51 -0700

    Puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
-* main - main block
-*
-* Return; Always 0
-*/
+ * main - print a line of code using puts
+ *
+ * Return: 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ *
+ * Return: Always 0
+ * /
+ int main(voi)
+ {
+    
