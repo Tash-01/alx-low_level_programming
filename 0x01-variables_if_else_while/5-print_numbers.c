65b8427d57e1b2163399848af0690c8072f2f665
tash-01 <tashfernandes93@gmail.com>
2023-03-17 19:05:04 -0700

    print numberss

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - print all single dogit number of base 10
+ * Return: 0
+ */
+int main(void)
+{
+	char c = '0'
+		while (c <= '9')
+		{
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
