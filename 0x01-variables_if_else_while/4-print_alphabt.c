478301204eec14dd98cc9f89c1b44edec6abebed
tash-01 <tashfernandes93@gmail.com>
2023-03-17 17:59:01 -0700

    print alphabets

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,19 +5,18 @@
  * main - Print alphbets in lowercase, follows by a newline
  * Return: 0
  */
+int main(void)
 {
-	int main(avoid)
-}
-char low, e, q;
-e = 'e';
-q = 'q';
+	char ch = 'a';
+		while (ch <= 'z')
+	{
+		if (ch != 'e' && ch != 'q';
+				}
+				putchar(ch);
+				{
+				ch++;
+				}
+				putchar('\n');
+				return (0);
+				}
 
-        for (low = 'a'; low <= 'z'; low++);
-
-{
-	if (low != e && low != q);
-
-	putchar (low);
-}
-putchar ('\n')
-return (0);
