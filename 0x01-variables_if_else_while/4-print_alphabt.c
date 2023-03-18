1d091e9e6a57e46138ecf25c20b97a0588367520
tash-01 <tashfernandes93@gmail.com>
2023-03-18 13:40:12 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,21 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
  * main - Print alphbets in lowercase, follows by a newline
  * Return: 0
+ *
  */
 int main(void)
 {
-	char c = 'a';
+	char ch;
 
-	while (c <= 'z')
-	if (c != 'e' && c != 'q')
-				{
-				putchar(c);
-				}
-				c++;
-				}
-				putchar('\n');
-				return (0);
-				}
+	ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		ch++;
+	}
+	putchar('\n');
+
+	return (0);
+}
