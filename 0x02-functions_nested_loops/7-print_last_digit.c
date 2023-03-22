acd77c784a3a7c9947e660a7fbe7a2e469b2fc7b
tash-01 <tashfernandes93@gmail.com>
2023-03-22 05:49:00 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include "6-abs.c"
+
+/**
+ * print_last_digit - main function
+ *
+ * @n: to get the last digit
+ * Return: last digit of n
+ */
+int print_last_digit(int n)
+{
+	_putchar('0' + _abs(n % 10));
+	return (_abs(n % 10));
+}
