a4308d7918ce3b277ade18bca1ef933ac48fbfef
tash-01 <tashfernandes93@gmail.com>
2023-03-22 07:16:14 -0700

    5-sign.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - check the code
+ * print_alphabet_x10 - a function that prints 10 times the alphabet
  *
  * Return: Always 0.
  */
--- 0x02-functions_nested_loops/5-main.c
+++ 0x02-functions_nested_loops/5-main.c
@@ -26,5 +26,3 @@ int main(void)
 	_putchar('\n');
 	return (0);
 }
-
-
