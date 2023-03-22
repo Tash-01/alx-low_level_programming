c27c5bc3378c2f1bead5f80474f543a601535ac9
tash-01 <tashfernandes93@gmail.com>
2023-03-22 05:13:36 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - get value of an int
+ * @n: get a number to be evaluated
+ * Return: Always 0
+ */
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (n * -1);
+	}
+}
