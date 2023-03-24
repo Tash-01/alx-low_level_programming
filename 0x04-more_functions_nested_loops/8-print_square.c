9fa3bbb9c994199a129df88861577810dee41a1f
tash-01 <tashfernandes93@gmail.com>
2023-03-24 09:52:44 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square,followed by a new line
+ * @size: size of the square
+ */
+void print_square(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		_putchar('#');
+	}
+	_putchar('\n');
+}
---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the number from 1 tp 100, followed by a new line
+ * but for multiples of three prints Fizz instead of the number
+ * ans for the multiples of five prints Buzz
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 != 0)
+		{
+			prinyf("Fizz");
+		}
+		else if (i % 5 == 0 && i %  3 != 0)
+		{
+			printf("Buzz");
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i == 1)
+		{
+			printf("%d", i);
+		}
+		else
+		{
+			printf("%d", i);
+		}
+	}
+	printf("\n");
+	return (0);
+}
