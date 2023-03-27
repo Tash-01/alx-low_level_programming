4acb7fc03cc78fc90f189162c2a73db8546b507e
tash-01 <tashfernandes93@gmail.com>
2023-03-27 09:47:31 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - prints astring in reverse
+ * @s: string
+ * Return: 0
+ */
+void print_rev(char *s)
+{
+	int c = 0;
+
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+
+	for (c -= 1; c >= 0; c--)
+	{
+		_putchar(s[c]);
+	}
+	_putchar('\n');
+}
