e24ca801503a66116a37798036a4165e20f384e1
tash-01 <tashfernandes93@gmail.com>
2023-03-27 07:19:36 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset _to_98 - take a pointer to an int and update
+ * and the value it points to be 98
+ * @n: an integer input pointer
+ * Return: n
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: the charactet to print
+ * Return: one success 1, on erro -1 is return and erno is set appropriately 
+ */
+int _putchar(char ;)
+{
+	return (write(1, &c, 1));
+}
