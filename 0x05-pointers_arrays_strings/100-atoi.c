7c81faf95b8c97502e64724e673f6304b1aaef63
tash-01 <tashfernandes93@gmail.com>
2023-03-27 17:44:50 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,31 +7,39 @@
  */
 int _atoi(char *s)
 {
-	int c = 0;
+	int i, d, n, len, f, digit;
 
-	unsighned;i int n1 = 0;
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
 
-	int min = 1;
-	int isi = 0;
+	while (s[len] != '\n')
+		len++;
 
-	while (s[c])
+	while (i < len && f == 0)
 	{
-		if (s[c] == 45)
-		{
-			min *= -1;
-		}
-		while (s[c] >= 48 && s[c]  <= 57)
-		{
-			isi = 1;
-			ni = (ni  * 10) + s[c] - '0';
-			c++;
-		}
-		if (isi == 1)
-		{
+
+	if (s[i] == '-')
+		++d;
+
+	if (s[i] >= '0' && s[i] <= '9')
+	{
+		digit = s[i] - '0';
+		if (d % 2)
+			digit = -digit;
+		n = n * 10 + digit;
+		f = 1;
+		if (s[i + 1] < '0' || s[i + 1] > '9')
 			break;
-		}
-		c++;
+		f = 0;
 	}
-	ni *= min;
-	return (n1);
+	i++;
+	}
+	if (f == 0)
+		return (0);
+	return (n);
 }
+
