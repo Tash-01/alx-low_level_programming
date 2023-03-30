53e8240ecd706eeee09168c475a7574118a94d22
tash-01 <tashfernandes93@gmail.com>
2023-03-30 14:35:42 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,18 +8,20 @@
  */
 void print_number(int n)
 {
-	unsigned int x
+	unsigned int n1;
+
+	n1 = n;
 
 	if (n < 0)
 	{
 		_putchar('-');
-		n *= -1;
+		n = -n;
 	}
 
-	x = n;
+	if (n1 / 10 != 0)
+	{
+		print_number(n1 / 10);
+	}
 
-	if (x / 10)
-		print_number(x / 10);
-
-	_putchar(x % 10 + '0');
+	_putchar((n1 % 10) + '0');
 }
