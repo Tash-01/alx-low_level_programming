5dee5aae7dbb397eae49afd3948344bb80bd3ea4
tash-01 <tashfernandes93@gmail.com>
2023-03-30 11:21:34 -0700

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -12,12 +12,12 @@ char *rot13(char *s)
 	char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
-		for (i = 0; *(s + i); i++);;
+		for (i = 0; *(s + i); i++)
 		{
 			for (j = 0; j < 52; j++)
 			{
 				if (a[j] == *(s + i))
-				{
+
 					*(s + i) = b[j];
 					break;
 				}
