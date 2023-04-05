1e0ddda6a6e759e4474a8514124ce341765f64c3
tash-01 <tashfernandes93@gmail.com>
2023-04-05 12:23:19 -0700

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - A function that retains tye length of a string
+ * @s: length of string
+ *
+ *Return: Always 0
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+
+		return (0);
+	}
+	s++;
+	return (_strlen_recursion(s + 1));
+}
