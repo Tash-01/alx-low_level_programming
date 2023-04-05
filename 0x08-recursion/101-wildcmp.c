4cf2e0b8dc5431f343dbccb1437013f91a790f18
tash-01 <tashfernandes93@gmail.com>
2023-04-05 13:51:22 -0700

    101-wildcmp.c

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * wildcmp - function that compares two strings and returns 1
+ * @s1: first string to compare
+ * @s2: second string to compare
+ * Return: Always 0
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0')
+	{
+		if (*s2 != '\0' && *s2 == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		return (*s2 == '\0');
+	}
+
+	if (*s2 == '\0')
+	{
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (0);
+}
---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - function that returns the natural square root of num
+ * @n: A number to calculate the natural square root
+ * Return: the natural square root
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (-1);
+	return (_sqrt_recursion(n, 0));
+}
+/**
+ * _sqrt_recursion - function that returns the natural square root of num
+ * @n:  A number to calculate the natural square root
+ * 
