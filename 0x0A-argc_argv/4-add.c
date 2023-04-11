be711632ea3fbe2004d937268402e65f6e58aa42
tash-01 <tashfernandes93@gmail.com>
2023-04-10 19:33:03 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,11 +1,81 @@
 #include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 /**
- * main - program that adds positive numbers
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ *
+ * Return: int converted from string
+ */
+int _atoi(char *s)
+{
+	int i, d, n, len, f, digit;
+
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
+	{
+		if (s[i] == '_')
+			++d;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+
+				break;
+
+			f = 0;
+		}
+		i++;
+	}
+	if (f == 0)
+		return (0);
+	return (n);
+}
+/**
+ * main - adds two positive numbers
  * @argc: number of arguments
  * @argv: array of arguments
- * Return: Always 0
+ * Return: 0 success, 1 success
  */
+int main(int argc, char *argv[])
+{
+	int sum, num, i, l, f;
+
+	sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (l = 0; argv[i][l] != '\0'; l++)
+		{
+			if (argv[i][l] > '9' || argv[i][l] < '0')
+			{
+				puts("Error");
+				return (1);
+			}
+		}
+	}
+	for (f = 1; f < argc; f++)
+	{
+		num = _atoi(argv[f]);
+		if (num >= 0)
+		{
+			sum += num;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
