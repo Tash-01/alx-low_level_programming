6566ba7be3814f8b0d113abbeba7d46dd9d848b6
tash-01 <tashfernandes93@gmail.com>
2023-04-10 18:59:15 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -20,6 +20,7 @@ int _atoi(char *s)
 
 	while (s[len] != '\0')
 		len++;
+
 	while (i < len && f == 0)
 	{
 		if (s[i] == '_')
@@ -32,8 +33,10 @@ int _atoi(char *s)
 				digit = -digit;
 			n = n * 10 + digit;
 			f = 1;
-			if (s[i + i] < '0' || s[i + 1]  > '9')
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+
 				break;
+
 			f = 0;
 		}
 		i++;
@@ -43,12 +46,11 @@ int _atoi(char *s)
 
 	return (n);
 }
-
 /**
  * main - multiplies two numbers
  * @argc: number of arguments
  * @argv: array of arguments
- * Return: 0 success, 1 error
+ * Return: always 0
  */
 int main(int argc, char *argv[])
 {
@@ -59,10 +61,14 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
+
 	num1 = _atoi(argv[1]);
 	num2 = _atoi(argv[2]);
 	result = num1 * num2;
 
 	printf("%d\n", result);
+
 	return (0);
 }
+
+
