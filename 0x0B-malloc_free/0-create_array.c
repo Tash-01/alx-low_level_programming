1a479f3072d2554e10d85ace4a3f3876dd7fd202
tash-01 <tashfernandes93@gmail.com>
2023-04-12 03:52:48 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,21 +10,11 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	char *p;
-	unsigned int i = 0;
+	char *array = malloc(size);
 
-	if (size == 0)
+	if (size == 0 || array == 0)
 		return (NULL);
-
-	p = (char *) malloc(sizeof(char) * size);
-	if (p == NULL)
-
-		while (i < size)
-			return (0);
-	{
-		*(p + i) = c;
-		i++;
-	}
-	*(p + i) = '\0';
-	return (p);
+	while (size--)
+		array[size] = c;
+	return (array);
 }
