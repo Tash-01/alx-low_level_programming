eff8e5e59c1dc9c787c0ba6ff98538726f9636c8
tash-01 <tashfernandes93@gmail.com>
2023-04-14 01:30:31 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -3,122 +3,102 @@
 #include <stdio.h>
 
 /**
- * _is_zero - determines if any number is zero
- * @argv: argument vector.
+ * is_digit - check if strings contain a non-digit char
+ * @s: string to be evaluated
  *
- * Return: no return
+ * Return: 0 if a non-digit is found, 1 if found
  */
-void _is_zero(char *argv[])
+int is_digit(char *s)
 {
-	int i, isn1 = 1, isn2 = 1;
+	i = 0;
 
-	for (i = 0, argv[1][i]; i++)
-		if (argv[1][i] != '0')
-		{
-			isn1 = 0;
-			break;
-		}
-	for (i = 0; argv[2][i]; i++)
-		if (argv[2][i] != '0')
-		{
-			isn2 = 0;
-			break;
-		}
-	if (isn1 == 1 || isn2 == 1)
+	while (s[i])
 	{
-		printf("0\n");
-		exit(0);
+
+	if (s[i] < '0' || s[i] > '9')
+		return (0);
+	i++;
 	}
+	return (1);
 }
 
 /**
- * _initialize_array - set memory to zero in new array
- * @ar: char array
- * @lar: length of char array
+ * _strlen - returns the length of string
+ * @s: string to evaluate
  *
- * Return: pointer of char array
+ * Return: length of the string
  */
-char *_initialize_array(char *ar, int lar)
+int _strlen(char *s)
 {
 	int i = 0;
 
-	for (i = 0; i < lar; i++)
-		ar[i] = '0';
-	ar[lar] = '\0';
-	return (ar);
-}
-
-/**
- * _checknum - determines length of the number
- * and checks if number is in base 10
- * @argv: argument vector
- * @n: row of array
- *
- * Return: length of number
- */
-int _checknum(char *argv[], int n)
-{
-	 int ln;
-
-	for (ln = 0; argv[n][ln]; ln++)
-	if (!isdigit)
-	else
-	if (argv[n][ln])
+	while (s[i] != '\0')
 	{
-			 printf("Error\n");
-			 exit(98);
+		i++;
 	}
-	return (ln);
+	return (i);
 }
 
 /**
- * main - entry point
- * program that multiplies two positive numbers
+ * errors - handles errors for main
+ */
+void errors(void)
+{
+	printf("Error\n");
+	exit(98);
+}
+
+/**
+ * main - multiplies two positive numbers
  * @argc: number of arguments
- * @argv: argumen vector
+ * @argv: array of arguments
  *
- * Return: 0
+ * Return: always 0
  */
 int main(int argc, char *argv[])
 {
-	int ln1, ln2, lnout, add, addl, i, j, k, ca;
-	char *nout;
+	char *s1, *s2;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
-	if (argc != 3)
-		printf("Error\n"), exit(98);
-	ln1 = _checknum(argv, 1), ln2 = _checknum(argv, 2);
-	_is_zero(argv), inout = ln1 + ln2, nout = malloc(lnout + 1);
-	if (nout == NULL)
-		printf("Error\n"), exit(98);
-	nout = _initialize_array(nout, lnout);
-	k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+		errors();
 
-	for (; k >= 0; k--, i--)
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	len = len1 + len2 + 1;
+	result = malloc(sizeof(int) * len);
+	if (!result)
+		return (1);
+
+	for (i = 0; i <= len1 + len2; i++)
+		result[i] = 0;
+	
+	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
-		if (i < 0)
+		digit1 = s1[len1] - '0';
+		carry = 0;
+
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 		{
-			if (addl > 0)
-			{
-				add = (nout[k] - '0') + addl;
-				if (add > 9) nout[k - 1] = (add / 10) + '0';
-				nout[k] = (add % 10) + '0';
-			}
-			i = ln1 - 1, j--, addl = 0, ca++, k = lnout - (1 + ca);
+			digit2 = s2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+				result[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
 		}
-		if (j < 0)
-		{
-			if (nout[0] != '0')
-				break;
-			lnout--;
-			free(nout), nout = malloc(lnout + 1), nout = _initialize_array(nout, lnout);
-			k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
-		}
-		if (j >= 0)
-		{
-			add = ((argv[1][i] - '0') * (argv[2][j] - '0')) + (nout[k] - '0') + addl;
-			addl = add / 10, nout[k] = (add % 10) + '0';
-		}
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
 	}
-	printf("%s\n", nout);
+	for (i = 0; i < len - 1; i++)
+	{
+		if (result[i])
+			a = i;
+		if (a)
+			_putchar(result[i] + '0');
+	}
+	if (!a)
+		_putchar('0');
+	_putchar('\n');
+	free(result);
 	return (0);
 }
