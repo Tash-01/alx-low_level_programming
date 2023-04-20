a9391834c727125e84c4fb9fd5ff7d1413d59508
tash-01 <tashfernandes93@gmail.com>
2023-04-20 01:02:28 -0700

    1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -4,7 +4,7 @@
 
 /**
  * print_numbers - prints numbers, followed by a new line.
- * @seperator - the string to be printed between numbers
+ * @separator - the string to be printed between numbers
  * @n: number of intergers passed to the function
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
