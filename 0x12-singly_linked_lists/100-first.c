5966ff1c43a240e570828940a1664740217a387f
tash-01 <tashfernandes93@gmail.com>
2023-04-28 16:46:05 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,5 +1,14 @@
 #include "lists.h"
 #include <stdio.h>
 
+void first(void) __attribute__ ((constructor));
+
 /**
- * free_list - frees a linked list
+ * first - print is a sentence before the main
+ * the function is executed
+ */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
