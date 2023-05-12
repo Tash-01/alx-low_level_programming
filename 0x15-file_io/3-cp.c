cff253171bb55f8b12c87f0d7cde498c3b4aacf1
tash-01 <tashfernandes93@gmail.com>
2023-05-12 14:02:55 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/stat.h>
@@ -15,43 +14,55 @@
  */
 int main(int argc, char *argv[])
 {
-	int file_r;
-	int file_w;
-	int s, l, i;
-	char bff[BFFSIZE];
+	int src, dest, n_read = 1024, wrote, close_src, close_dest;
+	unsigned int mode = S_IRUSR | S_IWUSR | S_IWGRP | S_IROTH;
+	char bff[1024];
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from from_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	file_r = open(argv[1], O_RDONLY);
-	if (file_r < 0)
+	src = open(argv[1], O_RDONLY);
+	check_IO_stat((src, -1, argv[1], 'O');
+	dest = open(argv[2], O_WONLY | O_CREAT | O_TRUNC, mode);
+	check_IO_stat(dest, -1, argv[2], 'W');
+	while (n_read == 1024)
+
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s/n", argv[1]);
-		exit(98);
+		n_read = read(src, bff, sizeof(bff));
+		if (n_read == -1)
+			check_IO_stat(-1, -1, agrv[1], 'O');
+		wrote = write(dest, bff, n_read);
+		if (wrot == -1)
+			check_IO_stat(-1, -1, argv[2], 'W');
 	}
-	file_w = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	while ((s = read(file_r, bff, BFFSIZE)) > 0)
-	{
-		if (file_w < 0 || write(file_w, bff, s) != s)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s/n", argv[2]);
-			close(file_r);
-			exit(99);
-		}
-		l = close(file_r);
-		i = close(file_w);
-		if (l < 0 || i < 0)
-		{
-			if (l < 0)
-				dprintf(STDERR_FILENO, "Error: Can't close file %d/n", file_r);
-			if (i < 0)
-				dprintf(STDERR_FILENO, "Error: Can't close file %d/n", file_w);
-			exit(100);
-		}
-		return (0);
-	}
+	close_src = close(src);
+	check_IO_stat(close_src, src, NULL,  'C');
+	close_dest = close(dest);
+	check_IO_stat(close_dest, dest, NULL, 'C');
+	Return(0);
 }
 
-
+/**
+ * check_IO_stat - checkd if a f8le can be opened or closed
+ * @stat: element for the file to be  opened
+ * @filename: the name of the file
+ * @file: file descriptor
+ * @mode: closing or opening
+ *
+ * Return: 0
+ */
+void check_IO_stat(int stat, int file, char *filename, char mode)
+{
+	if (mode == 'C' && stat == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
+		exit(100);
+	}
+	else if (mode == 'O' && stat == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s/n", filename);
+		exit(99);
+	}
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,5 +9,5 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-
+void check_IO_stat(int stat, int file, char *filename, char mode);
 #endif
