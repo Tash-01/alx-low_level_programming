0e84f7c9c9831f1d74ba139ff3f8332b270bcd08
Tash-01 <tashfernandes93@gmail.com>
2023-06-15 05:22:48 -0700

    0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,17 +8,15 @@
  */
 size_t print_dlistint(const dlistint_t *h);
 {
-	size_t nodes = 0;
-
-	if (!h)
-		return (0);
+	size_t cont  = 0;
+	const dlistint_t *h;
 
 	while (h)
 	{
-		print("%d/n", h->n);
+		printf("%d/n", h->n);
 		h = h->next;
-		nodes++;
+		cont++;
 	}
 
-	return (nodes);
+	return (cont);
 }
