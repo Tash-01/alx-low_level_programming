afee076439689a70bfe07bc5d59d80cfebecf85a
Tash-01 <tashfernandes93@gmail.com>
2023-06-15 17:16:06 -0700

    100-password

---
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * f4 - finds the biggest number
+ * @usrn: username
+ * @len: length of username
+ * Return: the biggest number
+ */
+int f4(char *usrn, int len)
+{
